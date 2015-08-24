31d62f8fd268782c3582371aa9b41bcd6efac352
Ethan Lu <ethan.lu@qq.com>
2015-08-24 20:22:15 +0800

    init

--- LeanCloud.php
+++ LeanCloud.php
@@ -4,6 +4,7 @@ namespace mivan\leancloud;
 
 use Curl\Curl;
 use yii\base\Component;
+use yii\helpers\Json;
 
 /**
  * Lean Cloud Base
@@ -11,23 +12,74 @@ use yii\base\Component;
 class LeanCloud extends Component
 {
 
-    public $appId = 'gvjh0sv7hkxfdyj4nrb7pjrv1b5fymi1xln7xkpmrq6i8ldh';
+    public $appId;
 
-    public $appKey = '9441i3ojnalv28vn17rbfxl6eqn069210c21vdq8nukliy8z';
+    public $appKey;
 
-    public $masterKey = 'fn3ijhq7pcwlfw32ozoukulj23sqwdvpji7jha9dbtmbll0p';
+    public $masterKey;
+
+    public $env = 'dev';
 
     public $apiUrl = 'https://api.leancloud.cn';
 
-    private $_client;
+    /** @var  Curl */
+    protected $_client;
 
     public function init()
     {
         parent::init();
-//        $this->appId = \Yii::$app->params['leanCloud']['appId'];
-//        $this->appKey = \Yii::$app->params['leanCloud']['appKey'];
-//        $this->masterKey = \Yii::$app->params['leanCloud']['masterKey'];
-
         $this->_client = new Curl();
     }
+
+
+    /**
+     * 推送消息
+     * @param string $title    标题
+     * @param string $alert    内容
+     * @param string $url   链接
+     * @param string $where 推送条件
+     * @return null
+     */
+    public function push($title, $alert, $url = '', $where = '')
+    {
+
+        if(is_numeric($where)) {
+            $data['where'] = [
+                'userId' => (string)$where
+            ];
+        } elseif (is_array($where)) {
+            $data['where'] = $where;
+        } else {
+            return false;
+        }
+
+        $data['prod'] = $this->env;
+
+        $data['data'] = [
+            'title' => $title,
+            'alert' => $alert,
+            'sound' => 'cheering.caf',
+            'url' => $url,
+        ];
+
+        return $this->_request('/1.1/push', $data);
+    }
+
+
+    /**
+     * 请求服务器
+     * @param string $method
+     * @param $path
+     * @param $data
+     * @return null
+     */
+    protected function _request($path, $data, $method = 'POST')
+    {
+        $this->_client->setHeader('X-AVOSCloud-Application-Id', $this->appId);
+        $this->_client->setHeader('X-AVOSCloud-Application-Key', $this->appKey);
+        $this->_client->setHeader('Content-Type', 'application/json');
+        $this->_client->{$method}($this->apiUrl.$path, Json::encode($data));
+        return $this->_client->response;
+    }
+
 }
